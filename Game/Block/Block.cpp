30395373f5723a3f5b5d7c373843a033ae3f6924
k022g0011@g.neec.ac.jp <k022g0011@g.neec.ac.jp>
2023-11-29 22:11:05 +0900

    ブロックマネージャー作成途中

--- Game/Block/Block.cpp
+++ Game/Block/Block.cpp
@@ -2,6 +2,8 @@
 #include "Collision/ColliderShapes/ColliderShapeBox2D.h"
 #include "Collision/CollisionConfig.h"
 #include "Collision/CollisionManager.h"
+#include "ModelManager.h"
+#include "GlobalVariables/GlobalVariables.h"
 
 OutBlock::OutBlock(const Vector3& pos)
 {
@@ -55,4 +57,132 @@ void OutBlockManager::Update()
 	for (const std::unique_ptr<OutBlock>& outBlock : outBlocks_) {
 		outBlock->Update();
 	}
+}
+
+MarkBlock::MarkBlock(const Vector3& pos)
+{
+	shapeType_ = std::make_unique<ColliderShapeBox2D>(BaseColliderShapeType::ColliderType::COLLIDER);
+	collisionAttribute_ = 0x00000000;
+	collisionMask_ = 0x00000000;
+
+	for (int i = 0; i < EditInfo::EditEnumV2::V2COUNT; i++) {
+		editInfo_.v2Paras_.push_back(Vector2());
+	}
+
+	SetCollisionAttribute(kCollisionAttributeBlock);
+	SetCollisionMask(kCollisionAttributePlayer);
+
+	shapeType_->SetV2Info(Vector2{ pos.x,pos.y }, Vector2{ 1.0f,1.0f }, Vector2{ 0.0f,0.0f });
+
+	// main.cppのモデルマネージャーの番号
+	model_.reset(ModelManager::GetInstance()->GetBlockModel(17));
+
+	worldTransform_.Initialize();
+	worldTransform_.translate_ = pos;
+	worldTransform_.UpdateMatrix();
+	pos_ = pos;
+
+	isSet_ = true;
+	isChangeScale_ = false;
+
+	SetGlobalVariable();
+}
+
+void MarkBlock::Init(const Vector3& pos)
+{
+	worldTransform_.Reset();
+	worldTransform_.translate_ = pos;
+	worldTransform_.UpdateMatrix();
+	pos_ = pos;
+
+	isSet_ = true;
+	isChangeScale_ = false;
+}
+
+void MarkBlock::Update()
+{
+	ApplyGlobalVariable();
+
+	if (!isChangeScale_) {
+		SetCollider();
+	}
+
+	worldTransform_.UpdateMatrix();
+}
+
+void MarkBlock::Draw(const ViewProjection& viewProjection)
+{
+	model_->Draw(worldTransform_, viewProjection);
+}
+
+void MarkBlock::Reset()
+{
+	isSet_ = false;
+}
+
+void MarkBlock::OnCollision()
+{
+	isChangeScale_ = true;
+}
+
+void MarkBlock::SetCollider()
+{
+	CollisionManager::GetInstance()->SetCollider(this);
+}
+
+void MarkBlock::SetGlobalVariable()
+{
+	GlobalVariables* globalVariables = GlobalVariables::GetInstance();
+
+	globalVariables->CreateGroup(groupName_);
+
+	for (int i = 0; i < IInfoNames::kIInfoCount; i++) {
+		globalVariables->AddItem(groupName_, iInfoNames_[i], iInfo_[i]);
+	}
+
+	for (int i = 0; i < FInfoNames::kFInfoCount; i++) {
+		globalVariables->AddItem(groupName_, fInfoNames_[i], fInfo_[i]);
+	}
+
+	ApplyGlobalVariable();
+}
+
+void MarkBlock::ApplyGlobalVariable()
+{
+	GlobalVariables* globalVariables = GlobalVariables::GetInstance();
+
+	for (int i = 0; i < IInfoNames::kIInfoCount; i++) {
+		iInfo_[i] = globalVariables->GetIntValue(groupName_, iInfoNames_[i]);
+	}
+
+	for (int i = 0; i < FInfoNames::kFInfoCount; i++) {
+		fInfo_[i] = globalVariables->GetFloatValue(groupName_, fInfoNames_[i]);
+	}
+}
+
+MarkBlockManager* MarkBlockManager::GetInstance()
+{
+	static MarkBlockManager instance;
+	return &instance;
+}
+
+void MarkBlockManager::Init()
+{
+
+}
+
+void MarkBlockManager::CreateMarkBlock(const Vector3& pos)
+{
+}
+
+void MarkBlockManager::Update()
+{
+}
+
+void MarkBlockManager::Draw(const ViewProjection& viewProjection)
+{
+}
+
+void MarkBlockManager::Clear()
+{
 }
--- Game/Block/Block.h
+++ Game/Block/Block.h
@@ -4,19 +4,7 @@
 #include "Collision/Collider.h"
 #include <list>
 #include "Model.h"
-
-//class IBlock : public Collider
-//{
-//public:
-//
-//	virtual void Init(const Vector3& pos) = 0;
-//
-//	virtual void Update() = 0;
-//
-//protected:
-//
-//	Vector2 pos_;
-//};
+#include <array>
 
 class OutBlock : public Collider
 {
@@ -54,5 +42,93 @@ private:
 };
 
 
+
+class  MarkBlock : public Collider
+{
+public:
+	MarkBlock(const Vector3& pos);
+
+	void Init(const Vector3& pos);
+
+	void Update();
+
+	void Draw(const ViewProjection& viewProjection);
+
+	void Reset();
+
+private:
+	void OnCollision() override;
+
+	void SetCollider();
+
+	void SetGlobalVariable();
+
+	void ApplyGlobalVariable();
+
+private:
+
+	std::unique_ptr<Model> model_;
+	WorldTransform worldTransform_;
+
+	int countFrame_;
+
+	bool isSet_;
+	bool isChangeScale_;
+
+	Vector3 pos_;
+
+	const std::string groupName_ = "Block";
+
+	enum FInfoNames {
+		kMaxScale,
+		kFInfoCount,
+	};
+
+	std::string fInfoNames_[FInfoNames::kFInfoCount] = {
+		"最大の拡大スケール"
+	};
+
+	std::array<float, FInfoNames::kFInfoCount> fInfo_;
+
+	enum IInfoNames {
+		kBigScaleFrame,
+		kSmallScaleFrame,
+		kIInfoCount,
+	};
+
+	std::string iInfoNames_[IInfoNames::kIInfoCount] = {
+		"スケール拡大のフレーム数",
+		"スケール縮小のフレーム数"
+	};
+
+	std::array<int, IInfoNames::kIInfoCount> iInfo_;
+	
+};
+
+class  MarkBlockManager
+{
+public:
+	static  MarkBlockManager* GetInstance();
+
+	void Init();
+
+	void CreateMarkBlock(const Vector3& pos);
+
+	void Update();
+
+	void Draw(const ViewProjection& viewProjection);
+
+	void Clear();
+
+private:
+	MarkBlockManager() = default;
+	~MarkBlockManager() = default;
+	MarkBlockManager(const MarkBlockManager&) = delete;
+	const MarkBlockManager& operator=(const MarkBlockManager&) = delete;
+
+private:
+
+	std::list<std::unique_ptr<MarkBlock>> markBlocks_;
+};
 
 
