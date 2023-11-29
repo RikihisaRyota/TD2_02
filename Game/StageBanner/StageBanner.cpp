a4e378ecffdfc7c91bead900a2c5e15df805aa0f
RikihisaRyota <115054910+RikihisaRyota@users.noreply.github.com>
2023-11-30 00:03:14 +0900

    長押し可能のテクスチャ追加

--- Game/StageBanner/StageBanner.cpp
+++ Game/StageBanner/StageBanner.cpp
@@ -5,6 +5,9 @@
 #include "MyMath.h"
 #include "SceneSystem/IScene/IScene.h"
 
+#include "Input.h"
+#include "ImGuiManager.h"
+
 StageBanner::StageBanner() {
 	stageTextureHandle_[MapChip::Stage::kStage_1] = TextureManager::Load("Resources/Textures/startStage1.png");
 	stageTextureHandle_[MapChip::Stage::kStage_2] = TextureManager::Load("Resources/Textures/startStage2.png");
@@ -24,22 +27,37 @@ StageBanner::StageBanner() {
 	stageTextureHandle_[MapChip::Stage::kStage_16] = TextureManager::Load("Resources/Textures/startStageEX.png");
 
 	frameTextureHandle_ = TextureManager::Load("Resources/Textures/startFram.png");
+	pressAndHoldTextureHandle_ = TextureManager::Load("Resources/Textures/longButton.png");
+
 	frameSize_.y *= 0.5f;
 	startPosition_ = { 1920.0f,360.0f };
 	endPosition_ = { -640.0f,360.0f };
 	kMaxAnimationCount_ = 120.0f;
 	stageSprite_.reset(Sprite::Create(stageTextureHandle_[MapChip::Stage::kStage_1], startPosition_, { 1.0f,1.0f,1.0f,1.0f }, { 0.5f,0.5f }));
 	frameSprite_.reset(Sprite::Create(frameTextureHandle_, startPosition_, { 1.0f,1.0f,1.0f,1.0f }, { 0.5f,0.5f }));
+	pressAndHoldSprite_.reset(Sprite::Create(pressAndHoldTextureHandle_, startPosition_, { 1.0f,1.0f,1.0f,1.0f }, { 0.5f,0.5f }));
+
 	stageSize_ = stageSprite_->GetSize();
 	frameSize_ = frameSprite_->GetSize();
+	pressAndHoldSize_ = pressAndHoldSprite_->GetSize();
 	frameSize_.y *= 0.5f;
 	frameSprite_->SetSize(frameSize_);
+	stagePosition_ = { 0.0f,0.0f };
+	pressAndHoldPosition_ = { 0.0f,390.0f };
 }
 
 void StageBanner::Initialize() {
 	isAnimation_ = true;
 	animationCount_ = 0.0f;
 	stageSprite_->SetTextureHandle(stageTextureHandle_[IScene::stageNo_]);
+	if (IScene::stageNo_ == MapChip::Stage::kStage_4 ||
+		IScene::stageNo_ == MapChip::Stage::kStage_9 ||
+		IScene::stageNo_ == MapChip::Stage::kStage_13) {
+		stagePosition_ = { 0.0f,334.0f };
+	}
+	else {
+		stagePosition_ = { 0.0f,360.0f };
+	}
 }
 
 void StageBanner::Update() {
@@ -49,15 +67,40 @@ void StageBanner::Update() {
 		t = 4.0f * std::pow((t - 0.5f), 3.0f) + 0.5f;
 		position_ = Lerp(startPosition_, endPosition_, t);
 
-		stageSprite_->SetPosition(position_);
+		stageSprite_->SetPosition({ position_.x,stagePosition_.y});
 		frameSprite_->SetPosition(position_);
+		pressAndHoldSprite_->SetPosition({ position_.x,pressAndHoldPosition_.y });
 		if (animationCount_ >= kMaxAnimationCount_) {
 			isAnimation_ = false;
 		}
 	}
+	if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
+		animationCount_ = 0.0f;
+		isAnimation_ = true;
+	}
+	ImGui::Begin("Debug");
+	if (ImGui::TreeNode("Banner")) {
+		ImGui::DragFloat2("start", &startPosition_.x, 1.0f);
+		ImGui::DragFloat2("end", &endPosition_.x, 1.0f);
+		ImGui::DragFloat2("stagePosition_", &stagePosition_.x, 1.0f);
+		ImGui::DragFloat2("pressAndHoldPosition_", &pressAndHoldPosition_.x, 1.0f);
+		ImGui::DragFloat2("stageSize", &stageSize_.x, 1.0f);
+		ImGui::DragFloat2("frameSize", &frameSize_.x, 1.0f);
+		ImGui::DragFloat2("pressAndHoldSize", &pressAndHoldSize_.x, 1.0f);
+		/*stageSprite_->SetPosition(stagePosition_);
+		frameSprite_->SetPosition(position_);
+		pressAndHoldSprite_->SetPosition(pressAndHoldPosition_);*/
+		ImGui::TreePop();
+	}
+	ImGui::End();
 }
 
 void StageBanner::Draw() {
 	frameSprite_->Draw();
 	stageSprite_->Draw();
+	if (IScene::stageNo_ == MapChip::Stage::kStage_4 ||
+		IScene::stageNo_ == MapChip::Stage::kStage_9 ||
+		IScene::stageNo_ == MapChip::Stage::kStage_13) {
+		pressAndHoldSprite_->Draw();
+	}
 }
--- Game/StageBanner/StageBanner.h
+++ Game/StageBanner/StageBanner.h
@@ -16,14 +16,22 @@ public:
 private:
 	uint32_t stageTextureHandle_[MapChip::Stage::kCount];
 	uint32_t frameTextureHandle_;
+	uint32_t pressAndHoldTextureHandle_;
+
 	std::unique_ptr<Sprite> stageSprite_;
 	std::unique_ptr<Sprite> frameSprite_;
+	std::unique_ptr<Sprite> pressAndHoldSprite_;
 	Vector2 position_;
+
+	Vector2 stagePosition_;
+	Vector2 pressAndHoldPosition_;
+	
 	Vector2 startPosition_;
 	Vector2 endPosition_;
 
 	Vector2 stageSize_;
 	Vector2 frameSize_;
+	Vector2 pressAndHoldSize_;
 
 	bool isAnimation_;
 	float animationCount_;
