8be5a1dfebcb6e55f96dfbfd249086f1bb7ea28f
k022g0011@g.neec.ac.jp <k022g0011@g.neec.ac.jp>
2023-11-30 09:56:15 +0900

    camera変更

--- SceneSystem/StageScene/StageScene.cpp
+++ SceneSystem/StageScene/StageScene.cpp
@@ -77,6 +77,7 @@ void StageScene::Init() {
 	timer_->Init();
 	stageUi_->Init();
 	stageBanner_->Initialize();
+	followCamera_->SetGoalPos(goal_->GetWorldTransform());
 }
 
 void StageScene::Reset() {
