510b098560a28e59c7c336a143bc2700610e801c
k022g0011@g.neec.ac.jp <k022g0011@g.neec.ac.jp>
2023-12-07 00:26:34 +0900

    下入力の受付を少し厳しくした。

--- Game/Player/Player.cpp
+++ Game/Player/Player.cpp
@@ -631,7 +631,7 @@ void Player::GripWallUpdate() {
 
 	}
 	else {
-		if (countFrame_ >= iParameters_[IParameterNames::kGripStayTime] || isDown_ || move.y <= -0.3f) {
+		if (countFrame_ >= iParameters_[IParameterNames::kGripStayTime] || isDown_ || move.y <= -0.5f) {
 			velocity_.y += parameters_[FloatParameterNames::kWallGravity];
 			isDown_ = true;
 		}
